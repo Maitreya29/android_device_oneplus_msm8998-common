8fe8f54aeba66fae49a2baf5691cd94ccf63da88
Maitreya29 <Maitreyapatni30@gmail.com>
2021-08-14 15:25:53 +0530

    msm8998-common: Setup Radiant flags

--- common.mk
+++ common.mk
@@ -504,9 +504,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     WfdCommon
 
-# NezukoOS
-NEZUKO_BUILD_TYPE := OFFICIAL
-NEZUKO_MAINTAINER := Maitreya29
+# Project Radiant
+RADIANT_BUILD_TYPE := OFFICIAL
 
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
