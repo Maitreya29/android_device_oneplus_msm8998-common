6446cee68d4b4199905d5579d4b9f6f9a2edf3db
Maitreya29 <Maitreyapatni30@gmail.com>
2021-09-07 13:39:10 +0530

    msm8998-common: Import kernel headers

--- common.mk
+++ common.mk
@@ -235,6 +235,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps/sap.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sap.conf \
     $(LOCAL_PATH)/configs/gps/xtwifi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/xtwifi.conf
 
+# Headers
+PRODUCT_VENDOR_KERNEL_HEADERS := device/oneplus/msm8998-common/kernel-headers
+
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl:64 \
